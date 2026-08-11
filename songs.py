b9005f541b8a0e168620721f62984595023d6c82
Jesse Huang <jessehuang2222@gmail.com>
2026-08-12 00:04:45 +0800

    Stored songs in list instead of array to allow same date

--- songs.py
+++ songs.py
@@ -1,50 +1,52 @@
 from datetime import date, timedelta
 import random
 
-SONGS = {
-    "2018-06-04" : ("秒針を噛む",                 "Byoushinwo Kamu",        "GJI4Gv7NbmE"),
-    "2018-10-02" : ("脳裏上のクラッカー",         "Nouriueno Cracker",      "3iAXclHlTTg"),
-    "2018-11-06" : ("ヒューマノイド",             "Humanoid",               "GAB26GgJ8V8"),
-    "2019-02-04" : ("眩しいDNAだけ",              "Mabushii DNA Dake",      "VJy8qZ77bpE"),
-    "2019-05-08" : ("正義",                       "Seigi",                  "7kUbX4DoZoc"),
-    "2019-09-26" : ("蹴っ飛ばした毛布",           "Kettobashita Moufu",     "iyCRK5WfFOI"),
-    "2019-10-08" : ("こんなこと騒動",             "Konnakoto Soudou",       "mlA-Z7zSLHU"),
-    "2019-10-18" : ("ハゼ馳せる果てるまで",       "Haze Haseru Haterumade", "ElnxZtiBDvs"),
-    "2019-10-29" : ("Dear Mr 「F」",              "Dear. Mr\"F\"",          "Qw-FSw7d2zE"),
-    "2020-03-06" : ("サターン(Acoustic ver.)",    "Saturn"  ,               "ubrovjZUi64"),
-    "2020-05-14" : ("お勉強しといてよ",           "Study Me",               "Atvsg_zogxo"),
-    "2020-07-13" : ("MILABO",                     "MILABO",                 "I88PrE-KUPk"),
-    "2020-07-23" : ("低血ボルト",                 "Fastening",              "COll6PdtI5w"),
-    "2020-08-11" : ("Ham",                        "Ham",                    "ouLndhBRL4w"),
-    "2020-12-01" : ("勘ぐれい",                   "Hunch Gray",             "ugpywe34_30"),
-    "2020-12-17" : ("正しくなれない",             "Can't be right",         "258qUAI7rck"),
-    "2021-01-20" : ("暗く黒く",                   "Darken",                 "dcOwj-QE_ZE"),
-    "2021-02-10" : ("胸の煙",                     "One's Mind",             "wQPgM-9LatM"),
-    "2021-03-30" : ("勘冴えて悔しいわ",           "Kan Saete Kuyashiiwa",   "4QePrv24TBU"),
-    "2021-06-18" : ("あいつら全員同窓会",         "Inside Joke",            "ZUwaudw8ht0"),
-    "2021-07-04" : ("ばかじゃないのに",           "Stay Foolish",           "YgmFIVOR1-I"),
-    "2021-12-02" : ("猫リセット",                 "Neko Reset",             "Sfz5TpCRSiI"),
-    "2022-02-16" : ("袖のキルト",                 "Quilt",                  "9PnCSI8ndws"),
-    "2022-04-07" : ("ミラーチューン",             "Mirror Tune",            "BVvvUGP0MFw"),
-    "2022-09-08" : ("消えてしまいそうです",       "Blush",                  "OxcnK1s2Fww"),
-    "2022-09-15" : ("夏枯れ",                     "Summer Slack",           "Nmemc-b6cdU"),
-    "2022-10-20" : ("残機",                       "Time Left",              "6OC92oxs4gA"),
-    "2022-12-15" : ("綺羅キラー",                 "Kira Killer",            "e5LaKxJVeVI"),
-    "2023-05-15" : ("不法侵入",                   "Intrusion",              "SAdkxVFyAyc"),
-    "2023-06-04" : ("花一匁",                     "Hanaichi Monnme",        "H88kps8X4Mk"),
-    "2023-08-20" : ("馴れ合いサーブ",             "Nareai Serve",           "bIW0n36TUSQ"),
-    "2024-05-23" : ("嘘じゃない",                 "Truth in Lies",          "GfDXqY-V0EY"),
-    "2024-06-06" : ("Blues in the Closet",        "Blues in the Closet",    "E8RMWLoAsa0"),
-    "2024-08-29" : ("海馬成長痛",                 "Hippocampal Pain",       "PLG2Uexyi9s"),
-    "2024-10-23" : ("クズリ念",                   "KUZURI",                 "ut889MZ9yNo"),
-    "2024-12-05" : ("TAIDADA",                    "TAIDADA",                "IeyCdm9WwXM"),
-    "2025-02-28" : ("シェードの埃は延長",         "SHADE",                  "zjEMFuj23B4"),
-    "2025-04-18" : ("微熱魔",                     "Warmthaholic",           "plpVOHqh3lA"),
-    "2025-05-22" : ("クリームで会いにいけますか", "CREAM",                  "JQ2913bVo30"),
-    "2025-06-12" : ("形",                         "Pain Give Form",         "6eFajRiOrpY"),
-    "2026-01-29" : ("メディアノーチェ",           "Medianoche",             "sBpITQ7oXxM"),
-    "2026-03-05" : ("よもすがら",                 "yomosugara",             "rsjaFk0Z5es"),
-}
+SONGS = [
+    ("2018-06-04", "秒針を噛む",                 "Byoushinwo Kamu",        "GJI4Gv7NbmE"),
+    ("2018-10-02", "脳裏上のクラッカー",         "Nouriueno Cracker",      "3iAXclHlTTg"),
+    ("2018-11-06", "ヒューマノイド",             "Humanoid",               "GAB26GgJ8V8"),
+    ("2019-02-04", "眩しいDNAだけ",              "Mabushii DNA Dake",      "VJy8qZ77bpE"),
+    ("2019-05-08", "正義",                       "Seigi",                  "7kUbX4DoZoc"),
+    ("2019-09-26", "蹴っ飛ばした毛布",           "Kettobashita Moufu",     "iyCRK5WfFOI"),
+    ("2019-10-08", "こんなこと騒動",             "Konnakoto Soudou",       "mlA-Z7zSLHU"),
+    ("2019-10-18", "ハゼ馳せる果てるまで",       "Haze Haseru Haterumade", "ElnxZtiBDvs"),
+    ("2019-10-29", "Dear Mr 「F」",              "Dear. Mr\"F\"",          "Qw-FSw7d2zE"),
+    ("2020-03-06", "サターン(Acoustic ver.)",    "Saturn"  ,               "ubrovjZUi64"),
+    ("2020-05-14", "お勉強しといてよ",           "Study Me",               "Atvsg_zogxo"),
+    ("2020-07-13", "MILABO",                     "MILABO",                 "I88PrE-KUPk"),
+    ("2020-07-23", "低血ボルト",                 "Fastening",              "COll6PdtI5w"),
+    ("2020-08-11", "Ham",                        "Ham",                    "ouLndhBRL4w"),
+    ("2020-12-01", "勘ぐれい",                   "Hunch Gray",             "ugpywe34_30"),
+    ("2020-12-17", "正しくなれない",             "Can't be right",         "258qUAI7rck"),
+    ("2021-01-20", "暗く黒く",                   "Darken",                 "dcOwj-QE_ZE"),
+    ("2021-02-10", "胸の煙",                     "One's Mind",             "wQPgM-9LatM"),
+    ("2021-03-30", "勘冴えて悔しいわ",           "Kan Saete Kuyashiiwa",   "4QePrv24TBU"),
+    ("2021-06-18", "あいつら全員同窓会",         "Inside Joke",            "ZUwaudw8ht0"),
+    ("2021-07-04", "ばかじゃないのに",           "Stay Foolish",           "YgmFIVOR1-I"),
+    ("2021-12-02", "猫リセット",                 "Neko Reset",             "Sfz5TpCRSiI"),
+    ("2022-02-16", "袖のキルト",                 "Quilt",                  "9PnCSI8ndws"),
+    ("2022-04-07", "ミラーチューン",             "Mirror Tune",            "BVvvUGP0MFw"),
+    ("2022-09-08", "消えてしまいそうです",       "Blush",                  "OxcnK1s2Fww"),
+    ("2022-09-15", "夏枯れ",                     "Summer Slack",           "Nmemc-b6cdU"),
+    ("2022-10-20", "残機",                       "Time Left",              "6OC92oxs4gA"),
+    ("2022-12-15", "綺羅キラー",                 "Kira Killer",            "e5LaKxJVeVI"),
+    ("2023-05-15", "不法侵入",                   "Intrusion",              "SAdkxVFyAyc"),
+    ("2023-06-04", "花一匁",                     "Hanaichi Monnme",        "H88kps8X4Mk"),
+    ("2023-08-20", "馴れ合いサーブ",             "Nareai Serve",           "bIW0n36TUSQ"),
+    ("2024-05-23", "嘘じゃない",                 "Truth in Lies",          "GfDXqY-V0EY"),
+    ("2024-06-06", "Blues in the Closet",        "Blues in the Closet",    "E8RMWLoAsa0"),
+    ("2024-08-29", "海馬成長痛",                 "Hippocampal Pain",       "PLG2Uexyi9s"),
+    ("2024-10-23", "クズリ念",                   "KUZURI",                 "ut889MZ9yNo"),
+    ("2024-12-05", "TAIDADA",                    "TAIDADA",                "IeyCdm9WwXM"),
+    ("2025-02-28", "シェードの埃は延長",         "SHADE",                  "zjEMFuj23B4"),
+    ("2025-04-18", "微熱魔",                     "Warmthaholic",           "plpVOHqh3lA"),
+    ("2025-05-22", "クリームで会いにいけますか", "CREAM",                  "JQ2913bVo30"),
+    ("2025-06-12", "形",                         "Pain Give Form",         "6eFajRiOrpY"),
+    ("2026-01-29", "メディアノーチェ",           "Medianoche",             "sBpITQ7oXxM"),
+    ("2026-03-05", "よもすがら",                 "yomosugara",             "rsjaFk0Z5es"),
+    ("2026-03-31", "ultra魂",                    "ultra soul",             "lpg5nhWapjU"),
+    ("2026-07-06", "イチジク煙",                 "Fig Smoke",              "4LDQXYTfWjA"),
+]
 
 class Song:
     def __init__(self, d, name, en_name, mv):
@@ -85,9 +87,8 @@ class Song:
 random.seed()
 songs = []
 
-for k, v in SONGS.items():
-    date = date.fromisoformat(k)
-    songs.append(Song(date, *v))
+for song_date, *song_data in SONGS:
+    songs.append(Song(date.fromisoformat(song_date), *song_data))
 
 class QueryResult(Song):
     def __init__(self, d, name, en_name, mv, adj_date):
@@ -123,19 +124,39 @@ def random_song():
             return Song("2009-10-25", "【統神端火鍋】高畫質修復", "", "dMTy6C4UiQ4")
     return random.choice(songs)
 
+# def random_seat():
+#     if random.randint(0, 100) < 5:
+#         if (rand := random.randint(0, 3)) == 0:
+#             return "你抽到的位子是保羅的大腿上"
+#         elif rand == 1:
+#             return "你落選了"
+#         else:
+#             return "你抽到了海軍陸戰隊！！"
+#     level = random.randint(1, 3)
+#     if random.randint(0, 99) < 5:
+#         level = 7
+#
+#     row = random.randint(1, 30)
+#     number = random.randint(1, 60)    
+#     return f"你抽到的位子是{level}階{row}列{number}番"
+
 def random_seat():
     if random.randint(0, 100) < 5:
-        if (rand := random.randint(0, 3)) == 0:
+        if (rand := random.randint(0, 4)) == 0:
             return "你抽到的位子是保羅的大腿上"
         elif rand == 1:
-            return "你落選了"
+            return "你抽到了大和西大寺站瞭望台"
+        elif rand == 2:
+            return "你抽到了近鉄奈良線快速急行"
         else:
             return "你抽到了海軍陸戰隊！！"
-    level = random.randint(1, 3)
-    if random.randint(0, 99) < 5:
-        level = 7
-
-    row = random.randint(1, 30)
-    number = random.randint(1, 60)    
-    return f"你抽到的位子是{level}階{row}列{number}番"
-
+    seat = random.randint(1, 30001)
+    if seat < 4001:
+        block = chr(ord('A') + seat // 1000)
+        num = seat % 1000
+        return f"你抽到的位子是 Premium Area {block} Block {num} 番"
+    else:
+        seat = seat - 4000
+        block = chr(ord('A') + seat // 1000)
+        num = seat % 1000
+        return f"你抽到的位子是 Standard Area {block} Block {num} 番"
